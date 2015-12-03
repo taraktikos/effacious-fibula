20da850aa1ba3e0de016380ab59897246b760f81
tsuhovenko <tsuhovenko@cogniance.com>
2015-12-03 12:24:25 +0200

    mongo auditing

--- src/main/java/com/user/persistence/entity/User.java
+++ src/main/java/com/user/persistence/entity/User.java
@@ -1,11 +1,19 @@
 package com.user.persistence.entity;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.joda.time.DateTime;
+import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Getter
-@Setter
+import java.util.Set;
+
+@Data
 @Builder
 @Document
 @NoArgsConstructor
@@ -16,15 +24,20 @@ public class User {
 
     @Id
     private String id;
+    @Indexed(unique = true)
     private String email;
     private String firstName;
     private String lastName;
     private String password;
-    private String role;
+    private Set<String> roles;
     private String photo;
+    @CreatedDate
+    private DateTime createdAt;
+    @LastModifiedDate
+    private DateTime updatedAt;
 
     public enum Roles {
-        ROLE_ADMIN, ROLE_USER
+        ROLE_SUPER_ADMIN, ROLE_ADMIN, ROLE_MANAGER, ROLE_USER
     }
 
 }
